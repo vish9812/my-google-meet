04c0f83b995d27bb3ea07ec969c79dfc0ed3ad99
Vishal Choudhary <vish9812@gmail.com>
2021-09-19 02:19:36 +0530

    Initial Commit - Create chat via SignalR

---
+++ src/VideoApp.Web/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoApp.Web.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task NewMessage(long username, string message)
+        {
+            await Clients.All.SendAsync("messageReceived", username, message);
+        }
+    }
+}
---
+++ src/VideoApp.Web/client-app/src/App.tsx
@@ -0,0 +1,48 @@
+import './App.css';
+
+import React, { createRef } from 'react';
+import { HubConnectionBuilder } from "@microsoft/signalr";
+
+const App = () => {
+  const inputField = createRef<HTMLInputElement>();
+  const username = new Date().getTime();
+
+  const connection = new HubConnectionBuilder()
+    .withUrl("https://localhost:5001/hub")
+    .build();
+
+  connection.on("messageReceived", (username: string, message: string) => {
+    console.log(`Received <<<<${message}>>>> from ${username}`);
+  });
+
+  connection.start().catch(err => console.error("Connection start error>>>>", err));
+
+  const handleMessageKeyUp = (event: any) => {
+    console.log("Change Message>>>>", event);
+    if (event.key === "Enter") {
+      send();
+    }
+  }
+
+  const send = () => {
+    console.log("Send called>>>", inputField.current!.value);
+    connection.send("newMessage", username, inputField.current!.value)
+      // .then(() => setMessage(""))
+      .catch(err => console.error("Sending Message Failed>>>>", err));
+  }
+
+  return (
+    <div className="App">
+      <div>User: {username}</div>
+      <div>
+        <input
+          type="text"
+          defaultValue="test"
+          onKeyUp={handleMessageKeyUp}
+          ref={inputField} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
